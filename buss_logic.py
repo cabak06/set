9aaf6f4854a701afb798b72b310f88aac078c5b3
Cahit Bakirci <cba@testhuset.dk>
2021-09-18 10:16:32 +0200

    first

---
+++ buss_logic.py
@@ -0,0 +1,15 @@
+
+def added(num1, num2):
+    return num1 + num2
+
+def subt(num1, num2):
+    return num1 -num2
+
+def multip(num1,num2):
+    return num1 * num2
+
+def divi(num1, num2):
+    return num1/num2
+
+
+greeting = 'Hello there'
---
+++ calc.py
@@ -0,0 +1,15 @@
+import buss_logic as bl
+
+
+num1 = 10
+num2 = 5
+
+adding = bl.added(num1,num2)
+
+subtracted = bl.subt(num1,num2)
+multiplied = bl.multip(num1,num2)
+division = bl.divi(num1,num2)
+
+print(adding, subtracted, multiplied, division)
+
+
