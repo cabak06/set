39b0cc531c45b89b3fcec9585a92a9148890b7bb
Cahit <cahit10@hotmail.com>
2021-09-20 22:10:23 +0200

    modified from unittest to pytest

--- testing.py
+++ testing.py
@@ -1,34 +1,27 @@
-import unittest
+import pytest
 import buss_logic as bl
 
 
 num1 = 20
 num2 = 10
 
-class TestCalcMethods(unittest.TestCase):
+
+def test_add():
+    expected = num1 + num2
+    actual = bl.added(num1, num2)
+    assert(expected, actual)
+
+def test_multi():
+    expected = num1 * num2
+    actual = bl.multip(num1, num2)
+    assert(expected, actual)
+
+def test_sub():        
+    expected = num1 - num2
+    actual = bl.subt(num1, num2)
+    assert(expected, actual)
     
-    def setUp(self):
-        self.num1 = 20
-        self.num2 = 10
-
-    def test_add(self):
-        expected = num1 + num2
-        actual = bl.added(num1, num2)
-        self.assertEqual(expected, actual)
-
-    def test_multi(self):
-        expected = num1 * num2
-        actual = bl.multip(num1, num2)
-        self.assertEqual(expected, actual)
-
-    def test_sub(self):        
-        expected = num1 - num2
-        actual = bl.subt(num1, num2)
-        self.assertEqual(expected, actual)
-    
-    def test_div(self):
-        expected = num1 / num2
-        actual = bl.divi(num1, num2)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_div():
+    expected = num1 / num2
+    actual = bl.divi(num1, num2)
+    assert(expected, actual)
